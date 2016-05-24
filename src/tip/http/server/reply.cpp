e6a3d3d5eef7e13e64fff4652ed68e00d182fd23
zmij <sergei.a.fedorov@gmail.com>
2016-05-24 16:22:15 +0300

    Resolve merge conflict

--- src/tip/http/server/reply.cpp
+++ src/tip/http/server/reply.cpp
@@ -24,108 +24,120 @@ namespace http {
 namespace server {
 
 struct reply::impl {
-	typedef std::set< cookie, cookie_name_cmp > cookies_type;
-	typedef boost::iostreams::stream_buffer<
-			boost::iostreams::back_insert_device< body_type >> vector_buff_type;
+    typedef std::set< cookie, cookie_name_cmp > cookies_type;
+    typedef boost::iostreams::stream_buffer<
+            boost::iostreams::back_insert_device< body_type >> vector_buff_type;
 
-	io_service_ptr		io_service_;
+    io_service_ptr        io_service_;
 
-	request_const_ptr	req_;
-	response_ptr		resp_;
-	send_response_func 	send_response_;
-	send_error_func		send_error_;
-	finished_func		finished_;
+    request_const_ptr    req_;
+    response_ptr        resp_;
+    send_response_func     send_response_;
+    send_error_func        send_error_;
+    finished_func        finished_;
 
-	bool				response_sent_;
+    bool                response_sent_;
 
-	vector_buff_type	output_buffer_;
-	std::ostream		output_stream_;
-	cookies_type		cookies_;
-	detail::context_registry registry_;
+    vector_buff_type    output_buffer_;
+    std::ostream        output_stream_;
+    cookies_type        cookies_;
+    detail::context_registry registry_;
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-	impl(io_service_ptr io_service, request_const_ptr req,
-			send_response_func sr, send_error_func se) :
-		io_service_(io_service), req_(req), resp_(new response{ {1,1} }),
-		send_response_(sr), send_error_(se), response_sent_(false),
-		output_buffer_(resp_->body_), output_stream_(&output_buffer_)
-	{
-	}
+    impl(io_service_ptr io_service, request_const_ptr req,
+            send_response_func sr, send_error_func se) :
+        io_service_(io_service), req_(req), resp_(new response{ {1,1} }),
+        send_response_(sr), send_error_(se), response_sent_(false),
+        output_buffer_(resp_->body_), output_stream_(&output_buffer_)
+    {
+    }
 #pragma GCC diagnostic pop
-	~impl()
-	{
-		if (!response_sent_ && send_error_) {
-			send_error_(response_status::internal_server_error);
-		}
-	}
+    ~impl()
+    {
+        if (!response_sent_ && send_error_) {
+            send_error_(response_status::internal_server_error);
+        }
+        if (finished_) {
+            try {
+                finished_();
+            } catch (...) {}
+            finished_ = nullptr;
+        }
+    }
 
-	void
-	add_cookie(cookie&& c)
-	{
-		if (!response_sent_) {
-			cookies_.insert(std::move(c));
-		}
-	}
+    void
+    add_cookie(cookie&& c)
+    {
+        if (!response_sent_) {
+            cookies_.insert(std::move(c));
+        }
+    }
 
-	void
-	add_cookie(cookie const& c)
-	{
-		add_cookie(cookie(c));
-	}
+    void
+    add_cookie(cookie const& c)
+    {
+        add_cookie(cookie(c));
+    }
 
-	void
-	remove_cookie(std::string const& name)
-	{
-		using namespace boost::posix_time;
-		using namespace boost::date_time;
-		using boost::gregorian::date;
-		cookie c{
-			name, "~deleted~",
-			ptime( date(1970, Jan, 1) )
-		};
-		add_cookie(std::move(c));
-	}
+    void
+    remove_cookie(std::string const& name)
+    {
+        using namespace boost::posix_time;
+        using namespace boost::date_time;
+        using boost::gregorian::date;
+        cookie c{
+            name, "~deleted~",
+            ptime( date(1970, Jan, 1) )
+        };
+        add_cookie(std::move(c));
+    }
 
-	void
-	done(response_status status)
-	{
-		registry_.clear();
-		output_stream_.flush();
-		resp_->set_status(status);
-		for (auto c: cookies_) {
-			resp_->add_cookie(c);
-		}
-		if (send_response_) {
-			send_response_(resp_);
-			response_sent_ = true;
-		}
-		if (finished_) {
-			finished_();
-		}
-	}
-	void
-	send_error(response_status status)
-	{
-		if (send_error_) {
-			send_error_(status);
-			response_sent_ = true;
-		}
-		if (finished_) {
-			finished_();
-		}
-	}
+    void
+    done(response_status status)
+    {
+        registry_.clear();
+        output_stream_.flush();
+        resp_->set_status(status);
+        for (auto c: cookies_) {
+            resp_->add_cookie(c);
+        }
+        if (send_response_) {
+            send_response_(resp_);
+            response_sent_ = true;
+        }
+        if (finished_) {
+            try {
+                finished_();
+            } catch (...) {}
+            finished_ = nullptr;
+        }
+    }
+    void
+    send_error(response_status status)
+    {
+        if (send_error_) {
+            send_error_(status);
+            response_sent_ = true;
+        }
+        if (finished_) {
+            try {
+                finished_();
+            } catch (...) {}
+            finished_ = nullptr;
+        }
+    }
 };
 
 reply::reply( io_service_ptr io_service, request_const_ptr req,
-		send_response_func sr, send_error_func se) :
-	pimpl_(new impl{ io_service, req, sr, se })
+        send_response_func sr, send_error_func se) :
+    pimpl_(new impl{ io_service, req, sr, se })
 {
-	pimpl_->registry_.set_reply_impl(pimpl_);
+    pimpl_->registry_.set_reply_impl(pimpl_);
 
-	locale_manager& lmgr =
-			boost::asio::use_service< locale_manager >(*io_service);
-	lmgr.deduce_locale(*this);
+    locale_manager& lmgr =
+            boost::asio::use_service< locale_manager >(*io_service);
+    lmgr.deduce_locale(*this);
 }
 
 reply::reply(pimpl pi) : pimpl_(pi)
@@ -135,138 +147,138 @@ reply::reply(pimpl pi) : pimpl_(pi)
 reply::io_service_ptr
 reply::io_service() const
 {
-	return pimpl_->io_service_;
+    return pimpl_->io_service_;
 }
 
 request_const_ptr
 reply::request() const
 {
-	return pimpl_->req_;
+    return pimpl_->req_;
 }
 
 request_method
 reply::method() const
 {
-	return pimpl_->req_->method;
+    return pimpl_->req_->method;
 }
 
 tip::iri::path const&
 reply::path() const
 {
-	return pimpl_->req_->path;
+    return pimpl_->req_->path;
 }
 
 headers const&
 reply::request_headers() const
 {
-	return pimpl_->req_->headers_;
+    return pimpl_->req_->headers_;
 }
 
 reply::body_type const&
 reply::request_body() const
 {
-	return pimpl_->req_->body_;
+    return pimpl_->req_->body_;
 }
 
 void
 reply::add_header(header const& h)
 {
-	pimpl_->resp_->add_header(h);
+    pimpl_->resp_->add_header(h);
 }
 
 void
 reply::add_header(header && h)
 {
-	pimpl_->resp_->add_header(std::move(h));
+    pimpl_->resp_->add_header(std::move(h));
 }
 
 void
 reply::add_cookie(std::string const& name, std::string const& value)
 {
-	cookie c { name, value };
-	pimpl_->add_cookie(std::move(c));
+    cookie c { name, value };
+    pimpl_->add_cookie(std::move(c));
 }
 
 void
 reply::add_cookie(cookie const& c)
 {
-	pimpl_->add_cookie(c);
+    pimpl_->add_cookie(c);
 }
 
 void
 reply::add_cookie(cookie && c)
 {
-	pimpl_->add_cookie(std::move(c));
+    pimpl_->add_cookie(std::move(c));
 }
 
 void
 reply::remove_cookie(std::string const& name)
 {
-	pimpl_->remove_cookie(name);
+    pimpl_->remove_cookie(name);
 }
 
 reply::body_type&
 reply::response_body()
 {
-	return pimpl_->resp_->body_;
+    return pimpl_->resp_->body_;
 }
 
 reply::body_type const&
 reply::response_body() const
 {
-	return pimpl_->resp_->body_;
+    return pimpl_->resp_->body_;
 }
 
 std::ostream&
 reply::response_stream()
 {
-	return pimpl_->output_stream_;
+    return pimpl_->output_stream_;
 }
 
 std::locale
 reply::get_locale() const
 {
-	return pimpl_->output_stream_.getloc();
+    return pimpl_->output_stream_.getloc();
 }
 
 std::locale
 reply::set_locale(std::locale const& loc)
 {
-	return pimpl_->output_stream_.imbue(loc);
+    return pimpl_->output_stream_.imbue(loc);
 }
 
 void
 reply::done(response_status status)
 {
-	pimpl_->done(status);
+    pimpl_->done(status);
 }
 
 void
 reply::client_error(response_status status)
 {
-	pimpl_->send_error(status);
+    pimpl_->send_error(status);
 }
 
 void
 reply::server_error(response_status status)
 {
-	pimpl_->send_error(status);
+    pimpl_->send_error(status);
 }
 
 void
 reply::on_finish(finished_func f)
 {
-	pimpl_->finished_ = f;
+    pimpl_->finished_ = f;
 }
 
 detail::context_registry&
 reply::context_registry()
 {
-	return pimpl_->registry_;
+    return pimpl_->registry_;
 }
 
 reply::context::context( reply const& r ) :
-		next_(nullptr), wpimpl_(r.pimpl_)
+        next_(nullptr), wpimpl_(r.pimpl_)
 {
 }
 
@@ -277,21 +289,21 @@ reply::context::~context()
 request_const_ptr
 reply::context::get_request() const
 {
-	reply::pimpl pi = wpimpl_.lock();
-	if (!pi) {
-		throw std::runtime_error("Reply already destroyed");
-	}
-	return pi->req_;
+    reply::pimpl pi = wpimpl_.lock();
+    if (!pi) {
+        throw std::runtime_error("Reply already destroyed");
+    }
+    return pi->req_;
 }
 
 reply
 reply::context::get_reply() const
 {
-	reply::pimpl pi = wpimpl_.lock();
-	if (!pi) {
-		throw std::runtime_error("Reply already destroyed");
-	}
-	return reply(pi);
+    reply::pimpl pi = wpimpl_.lock();
+    if (!pi) {
+        throw std::runtime_error("Reply already destroyed");
+    }
+    return reply(pi);
 }
 
 } /* namespace server */
