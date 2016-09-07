2e2703a07e9301051ea5a5c79b361181d6278033
zmij <sergei.a.fedorov@gmail.com>
2016-09-07 18:53:17 +0300

    AWM-3240 Case-insensitive http header parsing

--- include/tip/http/common/grammar/header_parse.hpp
+++ include/tip/http/common/grammar/header_parse.hpp
@@ -18,68 +18,70 @@ namespace grammar {
 namespace parse {
 
 struct known_header_name_grammar :
-		boost::spirit::qi::symbols<char, known_header_name> {
+        boost::spirit::qi::symbols<char, known_header_name> {
 
-	known_header_name_grammar();
+    known_header_name_grammar();
 };
 
 template < typename InputIterator >
 struct header_name_grammar :
-		boost::spirit::qi::grammar< InputIterator, header_name() > {
+        boost::spirit::qi::grammar< InputIterator, header_name() > {
 
-	header_name_grammar() : header_name_grammar::base_type(field_name)
-	{
-		using boost::spirit::qi::alnum;
-		using boost::spirit::qi::char_;
+    header_name_grammar() : header_name_grammar::base_type(field_name)
+    {
+        namespace qi = boost::spirit::qi;
+        using qi::alnum;
+        using qi::char_;
+        using qi::no_case;
 
-		field_name %= known_header | +(alnum | char_("-"));
-	}
+        field_name %= no_case[ known_header ] | +(alnum | char_("-"));
+    }
 
-	boost::spirit::qi::rule< InputIterator, header_name() > field_name;
-	known_header_name_grammar known_header;
+    boost::spirit::qi::rule< InputIterator, header_name() > field_name;
+    known_header_name_grammar known_header;
 };
 
 template < typename InputIterator >
 struct header_grammar :
-		boost::spirit::qi::grammar< InputIterator, header() > {
-	typedef header value_type;
-	typedef boost::spirit::context<
-			boost::fusion::cons< value_type&, boost::fusion::nil_ >,
-			boost::fusion::vector0<>
-		> context_type;
+        boost::spirit::qi::grammar< InputIterator, header() > {
+    typedef header value_type;
+    typedef boost::spirit::context<
+            boost::fusion::cons< value_type&, boost::fusion::nil_ >,
+            boost::fusion::vector0<>
+        > context_type;
 
-	struct value_ {
-		void
-		operator()(std::string const& v, context_type& ctx, bool&) const
-		{
-			boost::fusion::at_c<0>(ctx.attributes).second = v;
-		}
-	};
-	header_grammar() : header_grammar::base_type(key_value)
-	{
-		using boost::spirit::qi::space;
-		using boost::spirit::qi::char_;
-		using boost::spirit::qi::lit;
+    struct value_ {
+        void
+        operator()(std::string const& v, context_type& ctx, bool&) const
+        {
+            boost::fusion::at_c<0>(ctx.attributes).second = v;
+        }
+    };
+    header_grammar() : header_grammar::base_type(key_value)
+    {
+        using boost::spirit::qi::space;
+        using boost::spirit::qi::char_;
+        using boost::spirit::qi::lit;
 
-		value %= +(~char_("\r"));
-		key_value %= key >> *space >> lit(':')
-				>> *space >> value[value_()] >> lit("\r\n");
-	}
-	boost::spirit::qi::rule< InputIterator, header() > key_value;
-	header_name_grammar< InputIterator > key;
-	boost::spirit::qi::rule< InputIterator, std::string() > value;
+        value %= +(~char_("\r"));
+        key_value %= key >> *space >> lit(':')
+                >> *space >> value[value_()] >> lit("\r\n");
+    }
+    boost::spirit::qi::rule< InputIterator, header() > key_value;
+    header_name_grammar< InputIterator > key;
+    boost::spirit::qi::rule< InputIterator, std::string() > value;
 };
 
 template < typename InputIterator >
 struct headers_grammar :
-		boost::spirit::qi::grammar< InputIterator, headers() > {
-	headers_grammar() : headers_grammar::base_type(_headers)
-	{
-		_headers %= +_header;
-	}
+        boost::spirit::qi::grammar< InputIterator, headers() > {
+    headers_grammar() : headers_grammar::base_type(_headers)
+    {
+        _headers %= +_header;
+    }
 
-	boost::spirit::qi::rule< InputIterator, headers() > _headers;
-	header_grammar< InputIterator > _header;
+    boost::spirit::qi::rule< InputIterator, headers() > _headers;
+    header_grammar< InputIterator > _header;
 };
 
 }  // namespace parse
