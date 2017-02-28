de001dce19474dcf888350f631b42c133874f52c
zavyalov <zavialov@corp.mail.ru>
2017-02-28 17:31:36 +0300

    AWM-8358 localized errors

--- include/tip/http/server/error.hpp
+++ include/tip/http/server/error.hpp
@@ -12,17 +12,26 @@
 #include <tip/log.hpp>
 #include <tip/http/common/response_status.hpp>
 
+#include <boost/locale.hpp>
+
 namespace tip {
 namespace http {
 namespace server {
 
 class error: public std::runtime_error {
 public:
+	using localized_message         = ::boost::locale::message;
+public:
 	error(std::string const& cat, std::string const& w,
 			response_status s
 				= response_status::internal_server_error,
 			log::logger::event_severity sv = log::logger::ERROR) :
 		runtime_error(w), category_(cat), status_(s), severity_(sv) {};
+	error(std::string const& cat, localized_message const& w,
+			response_status s
+				= response_status::internal_server_error,
+			log::logger::event_severity sv = log::logger::ERROR) :
+		runtime_error(w.str()), category_(cat), status_(s), severity_(sv), message_l10n_(w), is_localized_(true) {};
 	virtual ~error() {}
 
 	virtual std::string const&
@@ -44,17 +53,32 @@ public:
 	status() const
 	{ return status_; }
 
+	localized_message const&
+	message_l10n() const
+	{ return message_l10n_; }
+
+	bool
+	is_localized() const
+	{ return is_localized_; }
+
 	virtual void
 	log_error(std::string const& message = "") const;
 private:
 	std::string						category_;
 	response_status					status_;
 	log::logger::event_severity		severity_;
+	localized_message               message_l10n_{};
+	bool                            is_localized_{false};
 };
 
 class client_error : public error {
 public:
 	client_error(std::string const& cat, std::string const& w,
+			response_status s
+				= response_status::bad_request,
+				log::logger::event_severity sv = log::logger::ERROR)
+		: error(cat, w, s, sv) {};
+	client_error(std::string const& cat, error::localized_message const& w,
 			response_status s
 				= response_status::bad_request,
 				log::logger::event_severity sv = log::logger::ERROR)
