577a0193ecfba0d47336229675a20e6f1b438708
zmij <sergei.a.fedorov@gmail.com>
2017-04-04 16:49:32 +0300

    Suppress unused-variable in boost headers

--- lib/ssl/include/tip/ssl_context_service.hpp
+++ lib/ssl/include/tip/ssl_context_service.hpp
@@ -8,8 +8,13 @@
 #ifndef TIP_SSL_SSL_CONTEXT_SERVICE_HPP_
 #define TIP_SSL_SSL_CONTEXT_SERVICE_HPP_
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/ssl.hpp>
+
+#pragma GCC diagnostic pop
 
 #include <memory>
 
@@ -27,30 +32,30 @@ namespace ssl {
 
 class ssl_context_service: public boost::asio::io_service::service {
 public:
-	typedef boost::asio::io_service io_service;
-	typedef io_service::service base_type;
-	typedef boost::asio::ssl::context context_type;
-	typedef boost::filesystem::path path_type;
+    typedef boost::asio::io_service io_service;
+    typedef io_service::service base_type;
+    typedef boost::asio::ssl::context context_type;
+    typedef boost::filesystem::path path_type;
 
-	static io_service::id id;
+    static io_service::id id;
 public:
-	ssl_context_service(io_service&);
-	virtual ~ssl_context_service();
+    ssl_context_service(io_service&);
+    virtual ~ssl_context_service();
 
-	context_type&
-	context();
-	context_type const&
-	context() const;
+    context_type&
+    context();
+    context_type const&
+    context() const;
 
-	void
-	load_verify_files(path_type const& directory);
+    void
+    load_verify_files(path_type const& directory);
 private:
-	virtual void
-	shutdown_service();
+    virtual void
+    shutdown_service();
 private:
-	struct impl;
-	typedef std::shared_ptr<impl> pimpl;
-	pimpl pimpl_;
+    struct impl;
+    typedef std::shared_ptr<impl> pimpl;
+    pimpl pimpl_;
 };
 
 } /* namespace ssl */
